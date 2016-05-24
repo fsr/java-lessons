a45fc5d95b16741b79ee5567daf5ce6bc41edd56
Uwe Schmidt <uwe-schmidt-dresden@gmx.de>
2016-05-24 20:04:49 +0200

    Give GameController somithing to work

---
+++ src/roguelike/src/main/java/de/tu/dresden/ifsr/kurs/java/roguelike/model/VisibleObject.java
@@ -0,0 +1,13 @@
+package de.tu.dresden.ifsr.kurs.java.roguelike.model;
+
+
+import de.tu.dresden.ifsr.kurs.java.roguelike.model.structures.Point;
+
+public interface VisibleObject {
+
+    public String getVisibleCharacter();
+
+    public Point getPosition();
+
+    public void setPosition(Point position);
+}
--- src/roguelike/src/main/java/de/tu/dresden/ifsr/kurs/java/roguelike/model/character/Enemy.java
+++ src/roguelike/src/main/java/de/tu/dresden/ifsr/kurs/java/roguelike/model/character/Enemy.java
@@ -4,12 +4,22 @@ import de.tu.dresden.ifsr.kurs.java.roguelike.model.Gender;
 
 public class Enemy extends Character {
 
+    private static final String charcter = "M";
+
     public Enemy(String name, Gender gender) {
         super(name, gender);
+
+        position.setX(10);
+        position.setY(7);
     }
 
     @Override
     public void fightAgain(Character fighter) {
         System.out.println("Ich kämpfe gegen einen Spieler.");
+    }
+
+    @Override
+    public String getVisibleCharacter() {
+        return charcter;
     }
 }
